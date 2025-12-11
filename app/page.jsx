7acdf5fdd0d877ce0fdc8d37021cee32d686d856
leonard manurung <leonard.manurung@leonxlab.digital>
2025-12-11 12:44:08 +0700

    Add Page component with iframe for external content

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Page(){
+    return (
+      <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
+        <iframe
+          src="http://152.42.193.140:23463/"
+          style={{ width: "100%", height: "100%", border: "none" }}
+            allow="fullscreen"
+          />
+      </div>
+    );
+}
